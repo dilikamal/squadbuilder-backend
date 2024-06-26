3948ff9a6ee0b07d76bbf4b8579cc02949c7fa18
dilikamal <146081176+dilikamal@users.noreply.github.com>
2024-06-26 13:35:15 +0200

    Environment variables set

--- src/main/java/htw/berlin/webtech/teambackend/web/TeamRepository.java
+++
@@ -1,8 +0,0 @@
-package htw.berlin.webtech.teambackend.web;
-
-import htw.berlin.webtech.teambackend.web.api.Team;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TeamRepository extends CrudRepository<Team, Long> { }
