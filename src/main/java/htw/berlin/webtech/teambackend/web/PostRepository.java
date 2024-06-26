edb29b39833f871e0c73a06b50faa360a187fae0
dilikamal <146081176+dilikamal@users.noreply.github.com>
2024-06-26 12:47:56 +0200

    26.6

--- src/main/java/htw/berlin/webtech/teambackend/web/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package htw.berlin.webtech.teambackend.web;
-
-import htw.berlin.webtech.teambackend.web.api.Post;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostRepository extends CrudRepository<Post, Long> {
-}
