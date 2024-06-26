80e5b5999cc171b7ea4a24e8550b8fc97a7b684b
dilikamal <146081176+dilikamal@users.noreply.github.com>
2024-06-26 15:50:28 +0200

    Get maps

--- src/main/java/htw/berlin/webtech/teambackend/web/TeamController.java
+++ src/main/java/htw/berlin/webtech/teambackend/web/TeamController.java
@@ -4,32 +4,11 @@ import htw.berlin.webtech.teambackend.web.api.Team;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/teams")
-public class TeamController {
+@Controller
+public class TeamController{
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(TeamController.class);
 
-    @Autowired
-    TeamService teamService;
-
-    @PostMapping
-    public Team createTeam(@RequestBody Team team) {
-        LOGGER.info("Creating team with tactic: {}, formation: {}, players: {}", team.getTactic(), team.getFormation(), team.getPlayers());
-        return teamService.save(team);
-    }
-
-    @GetMapping("/{id}")
-    public Team getTeam(@PathVariable Long id) {
-        LOGGER.info("Getting team with id: {}", id);
-        return teamService.get(id);
-    }
-
-    @GetMapping
-    public Iterable<Team> getAllTeams() {
-        LOGGER.info("Getting all teams");
-        return teamService.getAllTeams();
-    }
 }
