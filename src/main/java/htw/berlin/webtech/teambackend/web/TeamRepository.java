50d252811b5076a370827e75aed37b7f20abdc27
dilikamal <146081176+dilikamal@users.noreply.github.com>
2024-06-28 18:38:07 +0200

    Get maps

--- src/main/java/htw/berlin/webtech/teambackend/web/TeamRepository.java
+++ src/main/java/htw/berlin/webtech/teambackend/web/TeamRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TeamRepository extends CrudRepository<Team, Long> { }
+public interface TeamRepository extends CrudRepository<Team, Long> {
+
+
+}
