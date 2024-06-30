03cb7b6ce2077295233710a4fc543189b013a211
dilikamal <146081176+dilikamal@users.noreply.github.com>
2024-06-30 13:03:59 +0200

    Add untracked files

---
+++ src/main/java/htw/berlin/webtech/teambackend/web/TeamController.java
@@ -0,0 +1,50 @@
+package htw.berlin.webtech.teambackend.web;
+
+import htw.berlin.webtech.teambackend.web.api.Team;
+import htw.berlin.webtech.teambackend.web.TeamService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/teams")
+public class TeamController {
+
+    private final TeamService teamService;
+
+    public TeamController(TeamService teamService) {
+        this.teamService = teamService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Team>> getTeams() {
+        try {
+            List<Team> teams = teamService.getAllTeams();
+            return new ResponseEntity<>(teams, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Team> saveTeam(@RequestBody Team team) {
+        try {
+            Team savedTeam = teamService.save(team);
+            return new ResponseEntity<>(savedTeam, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTeam(@PathVariable Long id) {
+        try {
+            teamService.deleteTeam(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
---
+++ src/test/java/htw/berlin/webtech/teambackend/ThingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.berlin.webtech.teambackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
