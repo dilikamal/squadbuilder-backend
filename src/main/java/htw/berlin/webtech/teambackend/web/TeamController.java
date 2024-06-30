fc7c6d634dc50e16bb9d179a7b117b50c14ee2b8
dilikamal <146081176+dilikamal@users.noreply.github.com>
2024-06-30 17:30:00 +0200

    Get

--- src/main/java/htw/berlin/webtech/teambackend/web/TeamController.java
+++ src/main/java/htw/berlin/webtech/teambackend/web/TeamController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/teams")
 public class TeamController {
 
     private final TeamService teamService;
@@ -17,31 +18,11 @@ public class TeamController {
         this.teamService = teamService;
     }
 
-    @GetMapping("/teams")
+    @GetMapping
     public ResponseEntity<List<Team>> getTeams() {
         try {
             List<Team> teams = teamService.getAllTeams();
             return new ResponseEntity<>(teams, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @PostMapping("/teams-oa")
-    public ResponseEntity<Team> saveTeam(@RequestBody Team team) {
-        try {
-            Team savedTeam = teamService.save(team);
-            return new ResponseEntity<>(savedTeam, HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @DeleteMapping("teams/{id}")
-    public ResponseEntity<Void> deleteTeam(@PathVariable Long id) {
-        try {
-            teamService.deleteTeam(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
